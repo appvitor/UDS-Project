2fcdd03cc158233b614cf578479c3b18c7d0452d
vitor.correa <vitorcorrea11@hotmail.com>
2017-11-27 11:21:17 -0200

    Sistema compila, mas a Rota p/ produtos não funciona

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } 			from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProdutosComponent } 	from './produtos/produtos.component';
+
+import { ProdutosComponent } 		from './produtos/produtos.component';
+import { ProdutoDetalhesComponent } from './produto-detalhes/produto-detalhes.component';
 
 const routes: Routes = [
 	{ path: 'produtos', component: 'ProdutosComponent' }
 ];
 
 @NgModule({
-	exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProdutosComponent } from './produtos/produtos.component';
 import { ProdutoDetalhesComponent } from './produto-detalhes/produto-detalhes.component';
 import { ProdutoService } from './produto.service';
-import { AppRoutingModule } from './/app-routing.module'
+
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProdutosComponent,
-    ProdutoDetalhesComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
+  declarations: [
+    AppComponent,
+    ProdutosComponent,
+    ProdutoDetalhesComponent
+  ],
   providers: [ ProdutoService ],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
