cae35e4adaa9f8b197f1b9bfbd1c72b127703f96
vitor.correa <vitorcorrea11@hotmail.com>
2017-11-27 23:25:17 -0200

    Mock de dados atualizado com o novo formato da classe

--- src/app/mock-produtos.ts
+++ src/app/mock-produtos.ts
@@ -2,13 +2,13 @@ import {Produto} from './produto';
 
 export const PRODUTOS: Produto[] = [
 
-	{codigo: 2, nome: 'Banana', dataDeCompra: 11-25-2017, dataDeValidade: 11-27-2017, preco: 3.45},
-	{codigo: 3, nome: 'Uva', dataDeCompra: 11-25-2017, dataDeValidade: 11-27-2017, preco: 4.00}, 
-	{codigo: 4, nome: 'Pepino Japonês', dataDeCompra: 11-25-2017, dataDeValidade: 11-27-2017, preco: 4.10},
-	{codigo: 5, nome: 'Chocolate', dataDeCompra: 11-25-2017, dataDeValidade: 11-27-2017, preco: 5.00},
-	{codigo: 6, nome: 'Miojo', dataDeCompra: 11-25-2017, dataDeValidade: 11-27-2017, preco: 2.35},
-	{codigo: 7, nome: 'Bandeja de Sushi', dataDeCompra: 11-25-2017, dataDeValidade: 11-27-2017, preco: 9.70},
-	{codigo: 8, nome: 'Macarrão', dataDeCompra: 11-25-2017, dataDeValidade: 11-27-2017, preco: 4.20},
-	{codigo: 9, nome: 'Cereal', dataDeCompra: 11-25-2017, dataDeValidade: 11-27-2017, preco: 7.19},
-	{codigo: 10, nome: 'Peça de Salmão', dataDeCompra: 11-25-2017, dataDeValidade: 11-27-2017, preco: 23.95}
+	{codigo: 2, nome: 'Banana', precoUnitario: 3.45},
+	{codigo: 3, nome: 'Uva', precoUnitario: 4.00}, 
+	{codigo: 4, nome: 'Pepino Japonês', precoUnitario: 4.10},
+	{codigo: 5, nome: 'Chocolate', precoUnitario: 5.00},
+	{codigo: 6, nome: 'Miojo', precoUnitario: 2.35},
+	{codigo: 7, nome: 'Bandeja de Sushi', precoUnitario: 9.70},
+	{codigo: 8, nome: 'Macarrão', precoUnitario: 4.20},
+	{codigo: 9, nome: 'Cereal', precoUnitario: 7.19},
+	{codigo: 10, nome: 'Peça de Salmão', precoUnitario: 23.95}
 ];
