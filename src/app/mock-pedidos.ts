8caaca1bb41bf326282e391d84a3e1127fac07e1
vitor.correa <vitorcorrea11@hotmail.com>
2017-12-03 21:37:41 -0200

    Tentando fazer mock de pedidos funcionar

--- src/app/mock-pedidos.ts
+++ src/app/mock-pedidos.ts
@@ -1,10 +1,11 @@
-import { Pedido } 		from './pedido';
-import { Cliente } 		from './cliente';
-import { v4 as uuid } 	from 'uuid';
+import { Pedido } 			from './pedido';
+import { Cliente } 			from './cliente';
+import { ClienteService } 	from './cliente.service';
+import { v4 as uuid } 		from 'uuid';
 
 export const PEDIDOS: Pedido[] = [
 
-	{id: uuid(), numero: 1, cliente: { }, emissao: new Date(), total: 0 },
+	{id: uuid(), numero: 1, cliente: ClienteService.getClientes(), emissao: new Date(), total: 0 },
 	{id: uuid(), numero: 2, cliente: { }, emissao: new Date(), total: 0 }, 
 	{id: uuid(), numero: 3, cliente: { }, emissao: new Date(), total: 0 },
 	{id: uuid(), numero: 4, cliente: { }, emissao: new Date(), total: 0 },
